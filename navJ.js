b757a03196486af3408de5996172f6feee86fa53
mattj299 <mateonas586@gmail.com>
2020-02-06 16:56:12 -0800

    Temporary commit trying to get the menu items to slide down

--- navJ.js
+++ navJ.js
@@ -1,15 +1,17 @@
 var menuToggle = document.getElementsByClassName("menu-toggle")[0];
-
+var menu = document.getElementsByClassName("menu")[0];
 
 menuToggle.addEventListener("click", e => {
 
     if (menuToggle.classList.contains("active"))
     {
         menuToggle.classList.remove("active");
+        menu.classList.remove("active");;
     }
     else 
     {
         menuToggle.classList.add("active");
+        menu.classList.add("active");
     }
     
 })
